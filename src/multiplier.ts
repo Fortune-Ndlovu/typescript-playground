2da94fc1e3a073c8b6438f4c6a1a5ef6b0268ac6
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-05-24 00:45:22 +0100

    Accepting multiple possible values, using the union type and using the switch case statement for handling unexpected errors.

--- src/multiplier.ts
+++ src/multiplier.ts
@@ -1,5 +1,17 @@
-const multiplicator = (a: number, b: number, plainText: string) => { 
-    console.log(plainText, a * b);
+type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, op: Operation): number => { 
+    switch (op) { 
+        case 'multiply':
+            return a * b;
+        case 'add':
+            return a + b;
+        case 'divide':
+            if (b === 0) { throw new Error('Can\'t divide by 0!')}
+            return a / b;
+        default:
+            throw new Error('Operation is not multiply, add or divide');
+    }
 }
 
-multiplicator(7, 7, "7 x 7 = ");
+calculator(7, 7, 'divide');
