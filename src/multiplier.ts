711f0215a777491032d9983dc835de31153814e1
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-05-27 12:02:50 +0100

    Ensuring the parameters given are the right type.

--- src/multiplier.ts
+++ src/multiplier.ts
@@ -1,8 +1,35 @@
+interface MultiplyValues { 
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: string[]): MultiplyValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else { 
+        throw new Error('Provided values were not numbers!');
+    }
+ }
 
 const multiplier = (a: number, b: number, plainText: string): number => {
     let result = a * b;
     console.log(plainText, result);
     return result;
 }
- 
-multiplier(7, 7, "7 multiplied by 7 is ");
+
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    multiplier(value1, value2, `Multiplied ${value1} and ${value2}, the result is: `);
+} catch (error: unknown) {
+    let errorMessage = "Something went wrong";
+    if (error instanceof Error) {
+        errorMessage += ' error ' + error.message;
+    }
+    console.log(errorMessage);
+ }
